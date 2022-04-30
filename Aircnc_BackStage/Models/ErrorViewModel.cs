fc7271bf7636a8fa39dd0adacdcbd5a762b895be
Ron <mope3128@gmail.com>
2022-04-30 17:04:10 +0800

    init

---
+++ Aircnc_BackStage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aircnc_BackStage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
