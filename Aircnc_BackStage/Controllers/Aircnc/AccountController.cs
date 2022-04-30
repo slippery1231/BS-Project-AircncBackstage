1ee126af98f557d5a8702492d39e8d6318b21175
Ron <mope3128@gmail.com>
2022-04-30 18:40:18 +0800

    移入首頁

---
+++ Aircnc_BackStage/Controllers/Aircnc/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aircnc_BackStage.Controllers.Aircnc
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Home()
+        {
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
