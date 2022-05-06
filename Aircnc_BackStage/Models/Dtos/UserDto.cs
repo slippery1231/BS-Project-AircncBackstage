fcaa50dc99b36bc2961cb59552bd9a8e9188a401
Vicky <hsiaovicky567@gmail.com>
2022-05-06 14:13:13 +0800

    詳細顯示修正

--- Aircnc_BackStage/Models/Dtos/UserDto.cs
+++ Aircnc_BackStage/Models/Dtos/UserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,21 +9,22 @@ namespace Aircnc_BackStage.Models.Dtos
     public class UserDto
     {
         public int UserId { get; set; }
+        [Display(Name= "會員名稱")]
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
-        public string Password { get; set; }
+        //public string Password { get; set; }
         public DateTime? Birthday { get; set; }
-        public bool? Gender { get; set; }
-        public string Photo { get; set; }
+        public string? Gender { get; set; }
+        //public string Photo { get; set; }
         public string EmergencyContactName { get; set; }
         public string EmergencyContactPhone { get; set; }
         public string CreateTime { get; set; }
-        public int? UserVerificationId { get; set; }
-        public int? BankVerificationId { get; set; }
-        public bool? IsDelete { get; set; }
-        public bool? MailIsVerify { get; set; }
+        //public int? UserVerificationId { get; set; }
+        //public int? BankVerificationId { get; set; }
+        public string IsDelete { get; set; }
+        public string MailIsVerify { get; set; }
 
     }
 }
